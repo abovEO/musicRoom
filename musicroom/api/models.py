c3fd40b0eb4ee8c5e00146a439ad89f3bf3c7dae
Nainsi Das <dasnainsi@gmail.com>
2022-07-08 15:53:03 +0530

    Created models

--- musicroom/api/models.py
+++ musicroom/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class room(models.Model):
+    code = models.CharField(max_length=6, default="", unique=True)
+    host = models.CharField(max_length=20, unique=True)
+    guest_can_pause = models.BooleanField(null=False, default=False)
+    votes_to_skip = models.IntegerField(null=False, default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+
